25a0e92c334fb5262531ddc179bce6a79c6c602a
sheshnjak <sheshnjak@gmail.com>
2016-10-14 12:36:50 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,91 +1,79 @@
-var app = angular.module('App', ['angular.directives-round-progress']);
-app.controller('MainCtrl', function($scope, $interval) {
+var app = angular.module('App', []);
 
-/*/ roundProgress setup
-// round progress, osnovni primjer
-    $scope.roundProgressData = {
-        label: 0,
-        percentage: 0
-    };
-// Here I synchronize the value of label and percentage in order to have a nice demo
-    $scope.$watch('roundProgressData', function (newValue, oldValue) {
-      newValue.percentage = newValue.label / 100;
-    }, true);
-*/
-    
-// $scope.stpwtch model    
-    $scope.stpwtch = {
-        startTime: 0,
-        shotClock: 0,
-        gameClock: 0,
-        elapsedTime: 0,
-        restartTime: 0,
-        running: false,
-        label: 0,
-        percentage: 0,
-        
-        reset: function(){                          // resetiranje          
+app.controller('MainCtrl', function($scope, $interval) {
+    $scope.stpwtch = {      // $scope.stpwtch model
+        debug: '',          // debug message
+        startTime: 0,       // start timestamp (reset)
+        gameClock: 0,       // time offset from game start [ms]
+        shotClock: 0,       // time offset from attack start [ms]
+        elapsedTime: 0,     // internal variable
+        restartTime: 0,     // internal timestamp
+        running: false,     // running flag [true, false]
+        label: 0,           // stopwatch digital display value
+        status: 0,          // can be 0, 1 (yellow), 2 (red) depending on remaining .shotClock
+        rotateSVGbuttons: 0, //(window.innerWidth > window.innerHeight) ? 270 : 0,		// if it's a landscape, then rotate the buttons for 270°, if not "rotate by 0° "
+        viewBox: (window.innerWidth > window.innerHeight) ? "-500 200 1250 800" : "0 0 735 950",  // landscape/portrait switch - viewbox is different depending on orientation
+        reset: function(){
           $scope.stpwtch.startTime = new Date();
-          $scope.stpwtch.elapsedTime = $scope.stpwtch.gameClock = $scope.stpwtch.restartTime = 0;
+          $scope.stpwtch.elapsedTime = $scope.stpwtch.shotClock = $scope.stpwtch.restartTime = $scope.stpwtch.label = $scope.stpwtch.status = 0;
+        }, 
+        start: function(){
+            $scope.stpwtch.startTime = new Date();
+            $scope.stpwtch.running = $interval(function() {
+                var now = new Date();
+                $scope.stpwtch.elapsedTime = now - $scope.stpwtch.startTime;
+                $scope.stpwtch.shotClock = $scope.stpwtch.elapsedTime + $scope.stpwtch.restartTime;
+                
+                $scope.stpwtch.label = parseInt($scope.stpwtch.shotClock / 100).toFixed(2) / 10;  // round to 0.1 second
+                
+                if($scope.stpwtch.shotClock > 20 * 1000) {                     // shotClock running out
+                    $scope.stpwtch.audio();
+                    $scope.stpwtch.status = 1;
+                }
+                if($scope.stpwtch.shotClock > 30 * 1000) {                     // shotClock out
+                    $scope.stpwtch.stop();
+                    $scope.stpwtch.label = "I STEK!";
+                    $scope.stpwtch.status = 2;
+                }
+            }, 31);  // default 31 ms
         },
-        
-        toggle: function(){                     // toggle start/stop
-          if(!$scope.stpwtch.running){          // start   
-                $scope.stpwtch.startTime = new Date();
-                $scope.stpwtch.running = $interval(function() {
-                    var now = new Date();
-                    $scope.stpwtch.elapsedTime = now - $scope.stpwtch.startTime;
-                    $scope.stpwtch.gameClock = $scope.stpwtch.elapsedTime + $scope.stpwtch.restartTime;
-/* to $watch */                    $scope.stpwtch.label = parseInt($scope.stpwtch.gameClock / 1000);
-/* to $watch */                    $scope.stpwtch.percentage = $scope.stpwtch.label / 100*3.339;
-                }, 31);  // default 31 ms
-            } else {                             // stop
-                $interval.cancel($scope.stpwtch.running);
-                $scope.stpwtch.running = false;
-                $scope.stpwtch.restartTime += $scope.stpwtch.elapsedTime;
-                $scope.stpwtch.elapsedTime = 0;
-            }
+        stop: function(){
+            $interval.cancel($scope.stpwtch.running);
+            $scope.stpwtch.running = false;
+            $scope.stpwtch.restartTime += $scope.stpwtch.elapsedTime;
+            $scope.stpwtch.elapsedTime = 0;
+        },
+        toggle: function(){ $scope.stpwtch.running ? $scope.stpwtch.stop() : $scope.stpwtch.start(); }, 
+        audio: function(){ 
+          $scope.stpwtch.audio = document.createElement('audio');
+          $scope.stpwtch.audio.src = 'https://dl.dropboxusercontent.com/u/11038914/sheshnjak/stopwatch/CensorBeep.mp3';
+          $scope.stpwtch.audio.play();
         }
     };
+
 });
 
-/* izbrisati ispod
-app.directive('stopwatch', function() { return {
-  controller: function($scope, $interval) {
-    var self = this;
-    var totalElapsedMs = 0;
-    var elapsedMs = 0;
-    var startTime;
-    var timerPromise;
-    
-    self.start = function() {
-      if (!timerPromise) {
-        startTime = new Date();
-        timerPromise = $interval(function() {
-          var now = new Date();
-          elapsedMs = now.getTime() - startTime.getTime();
-        }, 31);
-      }
-    };
-    
-    self.stop = function() {
-      if (timerPromise) {
-        $interval.cancel(timerPromise);
-        timerPromise = undefined;
-        totalElapsedMs += elapsedMs;
-        elapsedMs = 0;
-        $scope.stpwtch.elapsedTime = 1;
-      }
-    };
+/*/ http://jsfiddle.net/spacm/HeMZP/   određivanje veličine viewporta
 
-    self.reset = function() {
-      startTime = new Date();
-      totalElapsedMs = elapsedMs = 0;
-    };
-    
-    self.getElapsedMs = function() {
-      return totalElapsedMs + elapsedMs;
-    };
-  }
-}});
-*/
+/* audio controller za zvučni alarm, uzeto iz http://www.ng-newsletter.com/posts/beginner2expert-scopes.html * /
+
+app.controller('PlayerController', ['$scope', function($scope) {
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = 'https://dl.dropboxusercontent.com/u/11038914/sheshnjak/stopwatch/CensorBeep.mp3';
+  $scope.play = function() {
+    $scope.audio.play();
+    $scope.playing = true;
+  };
+  $scope.stop = function() {
+    $scope.audio.pause();
+    $scope.playing = false;
+  };
+  $scope.audio.addEventListener('ended', function() {
+    $scope.$apply(function() {
+      $scope.stop();
+    });
+  });
+}]);
+
+//*/
--- readme.md
+++ readme.md
@@ -2,8 +2,3 @@ Waterpolo stopwatch created with angular.js. Ment to be used for tracking time o
 
 Vaterpolo štoperica napravljena pomoću angular.js za mjerenje vremena na vaterpolskim utakmicama.
 
-Štoperica napisana od 0, prilagođena za aplikaciju
-
-korištene skripte:
-http://www.directiv.es/angular-round-progress-directive
-
